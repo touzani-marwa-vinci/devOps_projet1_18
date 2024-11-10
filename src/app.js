191b60f3233bdea74294244d758ccf8e13e06e24
touzani-marwa-vinci <marwa.touzani@student.vinci.be>
2024-11-10 18:31:48 +0100

    refactoring_simpleLine

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ export class BerlinClock{
 
     oneMinuteLine(time){
         const minutes=parseInt(time.split(":")[1]);
-        if(minutes%10===0 ||minutes%10===5)return "aucune lampe allumée";
-        if(minutes%10===1 ||minutes%10===6)return "1ère lampe jaune allumée";
-        if(minutes%10===2 ||minutes%10===7)return "deux lampes jaune allumée";
-        if(minutes%10===3 ||minutes%10===8)return "trois lampes jaune allumée";
-        if(minutes%10===4 ||minutes%10===9)return "quatre lampes jaune allumée";
+
+        if(this.noLamp(minutes))return "aucune lampe allumée";
+
+        if(this.firsLamp(minutes))return "1ère lampe jaune allumée";
+
+        if(this.twoLamps(minutes))return "deux lampes jaune allumée";
+
+        if(this.threeLamps(minutes))return "trois lampes jaune allumée";
+
+        if(this.fourLamps(minutes))return "quatre lampes jaune allumée";
 
 
 
@@ -15,4 +20,24 @@ export class BerlinClock{
     }
 
 
+
+    fourLamps(minutes) {
+        return minutes % 10 === 4 || minutes % 10 === 9;
+    }
+
+    threeLamps(minutes) {
+        return minutes % 10 === 3 || minutes % 10 === 8;
+    }
+
+    twoLamps(minutes) {
+        return minutes % 10 === 2 || minutes % 10 === 7;
+    }
+
+    firstLamp(minutes) {
+        return minutes % 10 === 1 || minutes % 10 === 6;
+    }
+
+    noLamp(minutes) {
+        return minutes % 10 === 0 || minutes % 10 === 5;
+    }
 }
