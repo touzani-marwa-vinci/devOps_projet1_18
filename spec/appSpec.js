f730cd1d085c7bbc43639b2669a53f04b98688fa
touzani-marwa-vinci <marwa.touzani@student.vinci.be>
2024-11-10 18:13:22 +0100

    simple_line

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,5 +2,21 @@ import {BerlinClock} from "../src/app.js";
 
 describe("minute line",function(){
     const clock=new BerlinClock();
-}
-);
+
+    describe("simpleLine should return off when given 0:00",function(){
+        const time="0:00";
+
+        const result=clock.oneMinuteLine(time);
+
+        expect(result).toBe("aucune lampe allumée"); 
+    });
+     
+    it("1MinuteLine should return 1 lampe when given 0:01 ",function(){
+        const time="0:01";
+
+        const result=clock.oneMinuteLine(time);
+
+        expect(result).toBe("1ère lampe jaune allumée");
+    });
+
+});
--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 export class BerlinClock{
-    
+
+    oneMinuteLine(time){
+        const minutes=parseInt(time.split(":")[1]);
+        if(minutes%10===0 ||minutes%10===5)return "aucune lampe allumée";
+        if(minutes%10===1 ||minutes%10===6)return "1ère lampe jaune allumée";
+
+        
+    }
 
 
 }
