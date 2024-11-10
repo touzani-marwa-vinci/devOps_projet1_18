fd00e68a8ee10e87b0f5dbe95b78f27808966e70
touzani-marwa-vinci <marwa.touzani@student.vinci.be>
2024-11-10 18:25:42 +0100

    simple_line_2

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -3,7 +3,7 @@ import {BerlinClock} from "../src/app.js";
 describe("minute line",function(){
     const clock=new BerlinClock();
 
-    describe("simpleLine should return off when given 0:00",function(){
+    it("1MinuteLine should return off when given 0:00",function(){
         const time="0:00";
 
         const result=clock.oneMinuteLine(time);
@@ -17,6 +17,36 @@ describe("minute line",function(){
         const result=clock.oneMinuteLine(time);
 
         expect(result).toBe("1ère lampe jaune allumée");
+    });
+    it("1MinuteLine should return 2 lampes when given 0:02 ",function(){
+        const time="0:02";
+
+        const result=clock.oneMinuteLine(time);
+
+        expect(result).toBe("deux lampes jaune allumée");
+    });
+
+    it("1MinuteLine should return 3 lampes when given 0:03 ",function(){
+        const time="0:03";
+
+        const result=clock.oneMinuteLine(time);
+
+        expect(result).toBe("trois lampes jaune allumée");
+    });
+    
+    it("1MinuteLine should return 4 lampes when given 0:04 ",function(){
+        const time="0:04";
+
+        const result=clock.oneMinuteLine(time);
+
+        expect(result).toBe("quatre lampes jaune allumée");
+    });
+    it("1MinuteLine should return off when given 0:15 ",function(){
+        const time="0:15";
+
+        const result=clock.oneMinuteLine(time);
+
+        expect(result).toBe("aucune lampe allumée");
     });
 
 });
--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ export class BerlinClock{
         const minutes=parseInt(time.split(":")[1]);
         if(minutes%10===0 ||minutes%10===5)return "aucune lampe allumée";
         if(minutes%10===1 ||minutes%10===6)return "1ère lampe jaune allumée";
+        if(minutes%10===2 ||minutes%10===7)return "deux lampes jaune allumée";
+        if(minutes%10===3 ||minutes%10===8)return "trois lampes jaune allumée";
+        if(minutes%10===4 ||minutes%10===9)return "quatre lampes jaune allumée";
+
+
+
 
         
     }
