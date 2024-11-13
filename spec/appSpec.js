cc5fe6fa2bcd9b8373fa7f160700e94db73d3ba3
touzani-marwa-vinci <marwa.touzani@student.vinci.be>
2024-11-13 11:34:06 +0100

    affichage_test

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -7,6 +7,10 @@ describe("minute line", function () {
         const time = "0:00";
 
         const result = clock.oneMinuteLine(time);
+        
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
 
         expect(result).toBe("aucune lampe allumée");
     });
@@ -16,6 +20,10 @@ describe("minute line", function () {
 
         const result = clock.oneMinuteLine(time);
 
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("1ère lampe jaune allumée");
     });
 
@@ -23,6 +31,10 @@ describe("minute line", function () {
         const time = "0:02";
 
         const result = clock.oneMinuteLine(time);
+
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
 
         expect(result).toBe("deux lampes jaune allumée");
     });
@@ -32,6 +44,10 @@ describe("minute line", function () {
 
         const result = clock.oneMinuteLine(time);
 
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("trois lampes jaune allumée");
     });
 
@@ -40,6 +56,10 @@ describe("minute line", function () {
 
         const result = clock.oneMinuteLine(time);
 
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("quatre lampes jaune allumée");
     });
     it("1MinuteLine should return off when given 0:15 ", function () {
@@ -47,6 +67,10 @@ describe("minute line", function () {
 
         const result = clock.oneMinuteLine(time);
 
+        console.log("Test oneMinuteBlock:", time,":",result);
+
+        console.log("");
+        
         expect(result).toBe("aucune lampe allumée");
     });
 });
@@ -57,104 +81,144 @@ describe("the 5MinuteLine", function() {
     it("5MinuteLine should return no lamp when given 0:00", function() {
         const time = "0:00";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("Aucune lampe allumée");
     });
 
     it("5MinuteLine should return 1 lamp when given 0:05", function() {
         const time = "0:05";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune");
     });
 
     it("5MinuteLine should return 2 lamps when given 0:10", function() {
         const time = "0:10";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune");
     });
 
     it("5MinuteLine should return 3 lamps when given 0:15", function() {
         const time = "0:15";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge");
     });
 
     it("5MinuteLine should return 4 lamps when given 0:20", function() {
         const time = "0:20";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune");
     });
 
     it("5MinuteLine should return 5 lamps when given 0:25", function() {
         const time = "0:25";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune");
     });
 
     it("5MinuteLine should return 6 lamps when given 0:30", function() {
         const time = "0:30";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+        
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge");
     });
 
     it("5MinuteLine should return 7 lamps when given 0:35", function() {
         const time = "0:35";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune");
     });
 
     it("5MinuteLine should return 8 lamps when given 0:40", function() {
         const time = "0:40";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune");
     });
 
     it("5MinuteLine should return 9 lamps when given 0:45", function() {
         const time = "0:45";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge");
     });
 
     it("5MinuteLine should return 10 lamps when given 0:50", function() {
         const time = "0:50";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune");
     });
 
     it("5MinuteLine should return 11 lamps when given 0:55", function() {
         const time = "0:55";
         const result = clock.fiveMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune");
     });
 
     it("5MinuteLine should return 3 lamps of five minutes and 2 lamps of one minute when given 0:12", function() {
         const time = "0:12";
+
         const result = clock.fiveMinuteLine(time) + "\n" + clock.oneMinuteLine(time);
-        console.log("Test time:", time,":",result);
+
+        console.log("Test 5MinuteBlock:", time,":",result);
+
         console.log("");
+
         expect(result).toBe("lampe jaune, lampe jaune\ndeux lampes jaune allumée");
     });
 });
@@ -167,6 +231,11 @@ describe("the oneHoureLine", function () {
 
         const result = clock.oneHourLine(time);
 
+        console.log("Test oneHoursBlock:", time,":",result);
+
+        console.log("");
+
+
         expect(result).toBe("Aucune lampe allumée");
     });
 
@@ -174,6 +243,11 @@ describe("the oneHoureLine", function () {
         const time = "1:00";
 
         const result = clock.oneHourLine(time);
+
+        console.log("Test oneHoursBlock:", time,":",result);
+
+        console.log("");
+
 
         expect(result).toBe("une lampe rouge allumée");
     });
@@ -183,6 +257,11 @@ describe("the oneHoureLine", function () {
 
         const result = clock.oneHourLine(time);
 
+        console.log("Test oneHoursBlock:", time,":",result);
+
+        console.log("");
+
+
         expect(result).toBe("deux lampes rouge allumée");
     });
 
@@ -190,6 +269,10 @@ describe("the oneHoureLine", function () {
         const time = "3:00";
 
         const result = clock.oneHourLine(time);
+
+        console.log("Test oneHoursBlock:", time,":",result);
+
+        console.log("");
 
         expect(result).toBe("trois lampes rouge allumée");
     });
@@ -199,6 +282,10 @@ describe("the oneHoureLine", function () {
         const time = "4:00";
 
         const result = clock.oneHourLine(time);
+
+        console.log("Test oneHoursBlock:", time,":",result);
+
+        console.log("");
 
         expect(result).toBe("quatre lampes rouge allumée");
     });
@@ -213,6 +300,10 @@ describe("the fiveHoursLine", function () {
 
         const result = clock.fiveHourLine(time);
 
+        console.log("Test fiveHoursBlock:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("Aucune lampe allumée");
 
     });
@@ -222,6 +313,10 @@ describe("the fiveHoursLine", function () {
 
         const result = clock.fiveHourLine(time);
 
+        console.log("Test fiveHoursBlock:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("une lampe rouge allumée");
     });
 
@@ -229,6 +324,10 @@ describe("the fiveHoursLine", function () {
         const time = "10:00";
 
         const result = clock.fiveHourLine(time);
+
+        console.log("Test fiveHoursBlock:", time,":",result);
+
+        console.log("");
 
         expect(result).toBe("deux lampes rouge allumée");
     });
@@ -238,6 +337,10 @@ describe("the fiveHoursLine", function () {
 
         const result = clock.fiveHourLine(time);
 
+        console.log("Test fiveHoursBlock:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("trois lampes rouge allumée");
     });
 
@@ -245,6 +348,10 @@ describe("the fiveHoursLine", function () {
         const time = "20:00";
 
         const result = clock.fiveHourLine(time);
+
+        console.log("Test fiveHoursBlock:", time,":",result);
+
+        console.log("");
 
         expect(result).toBe("quatre lampes rouge allumée");
     });
@@ -258,6 +365,10 @@ describe("the secondsLine", function () {
 
         const result = clock.secondeLine(time);
 
+        console.log("Test secondes:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("lampe éteinte");
     });
 
@@ -266,6 +377,10 @@ describe("the secondsLine", function () {
 
         const result = clock.secondeLine(time);
 
+        console.log("Test secondes:", time,":",result);
+
+        console.log("");
+
         expect(result).toBe("lampe jaune allumée");
     });
 
@@ -273,6 +388,10 @@ describe("the secondsLine", function () {
         const time = "0:00:03";
 
         const result = clock.secondeLine(time);
+
+        console.log("Test secondes:", time,":",result);
+
+        console.log("");
 
         expect(result).toBe("lampe éteinte");
     });
@@ -285,7 +404,7 @@ describe("the berlinclock",function(){
         const time="0:00:00";
 
         const result=clock.berlinClock(time)
-        console.log("Test time:", time,":","[",result,"]");
+        console.log("Test berlin_cock:", time,":","[",result,"]");
         console.log("");
         expect(result).toBe("Aucune lampe allumée"+"\n"+"Aucune lampe allumée"+"\n"+"Aucune lampe allumée"+"\n"+"aucune lampe allumée"+"\n"+"lampe jaune allumée");
     });
@@ -294,7 +413,7 @@ describe("the berlinclock",function(){
          const time="19:42:01";
 
          const result=clock.berlinClock(time)
-         console.log("Test time:", time,":","[",result,"]");
+         console.log("Test berlin_clock:", time,":","[",result,"]");
          console.log("");
         // expect(result).toBe("trois lampes rouge allumée\nquatre lampes rouge allumée\n1er lampe jaune ,2ème lampe jaune,3ème lampe rouge,4ème lampe jaune,5ème lampe jaune,6ème lampe rouge,7ème lampe jaune,8ème lampe jaune\ndeux lampes jaune allumée\nlampe éteinte");
         expect(result).toBe("trois lampes rouge allumée"+"\n"+"quatre lampes rouge allumée"+"\n"+"lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune"+"\n"+"deux lampes jaune allumée"+"\n"+"lampe éteinte");
@@ -305,7 +424,7 @@ it("berlinClock should return the correct representation for 23:59:59", function
         const time = "23:59:59";
 
         const result = clock.berlinClock(time);
-        console.log("Test time:", time,":","[",result,"]");
+        console.log("Test berlin_clock:", time,":","[",result,"]");
         console.log("");
         // expect(result).toBe("quatre lampes rouge allumée\ntrois lampes rouge allumée\n1er lampe jaune ,2ème lampe jaune,3ème lampe rouge,4ème lampe jaune,5ème lampe jaune,6ème lampe rouge,7ème lampe jaune,8ème lampe jaune,9ème lampe rouge,10ème lampe jaune,11ème lampe jaune\nles quatre lampes jaune allumée\nlampe éteinte");
         expect(result).toBe("quatre lampes rouge allumée"+"\n"+"trois lampes rouge allumée"+"\n"+"lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune, lampe rouge, lampe jaune, lampe jaune"+"\n"+"quatre lampes jaune allumée"+"\n"+"lampe éteinte");
